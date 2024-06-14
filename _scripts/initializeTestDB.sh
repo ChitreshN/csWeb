55ed30266e95d757e05abd09ec05a58ff6ca8477
ChitreshN <chitreshnarra@gmail.com>
2024-06-14 12:17:45 +0530

    feat: script to handle name to email conversion

--- _scripts/initializeTestDB.sh
+++ _scripts/initializeTestDB.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
+
 psql -U postgres -d testdb -f ./_postgresDB/testdb.sql
 
 rm testInsert.sql
 LOC="./_data_csv"
-for name in `ls $LOC` 
+for name in `ls $LOC`
 do
-    echo processing $name
-    python3 ./_scripts/csv2psql.py $LOC/$name ${name%.*} > _insert/${name%.*}.sql ;
+    if [ "$name" = "btech.csv" ] || [ "$name" = "btechAlumni.csv" ]; then
+        echo processing $name
+        python3 ./_scripts/csv2psql_btech.py $LOC/$name ${name%.*} > _insert/${name%.*}.sql
+    else
+        echo processing $name
+        python3 ./_scripts/csv2psql.py $LOC/$name ${name%.*} > _insert/${name%.*}.sql
+    fi
 done
