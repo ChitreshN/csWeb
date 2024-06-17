1959d0029281dd62bcabe228a8896a31d9b4e870
ChitreshN <chitreshnarra@gmail.com>
2024-06-17 18:47:06 +0530

    feat: add master script to initialize database

---
+++ _scripts/main.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# download csv
+#./_scripts/downloadAllCSVs.sh
+
+# refresh db
+
+psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS testdb;"
+psql -U postgres -d postgres -c "create database testdb;"
+
+# initialize db and process files
+./_scripts/initializeTestDB.sh
+
+# populate db
+./_scripts/populateTestDB.sh
+
+# TODO test
+# ./_scripts/test.sh
+
+# start the server
+cd Development
+node index.js
